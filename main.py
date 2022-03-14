96d0612b9293e65b5fd841815672ec9553723e03
raphael.alcateia <raphael.alcateia@salcisne.com.br>
2022-03-14 16:50:19 -0300

    fatiamento de url

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dollar&quantidade=100"
+#url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dollar&quantidade=100"
+#print(url)
+
+url = 'bytebank.com/cambio?moedaOrigem=real'
+
+
+url_base = url[0:19]
+print(url_base)
+
+url_parametros = url[20:36]
+print(url_parametros)
+
 print(url)
