7a864a7888637e041ada00c13046e675c55b4c31
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2022-03-18 00:54:21 -0300

    fatiar as url sem usar posição fixa

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-#url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dollar&quantidade=100"
+url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dollar&quantidade=100"
 #print(url)
 
-url = 'bytebank.com/cambio?moedaOrigem=real'
+#url = 'bytebank.com/cambio?moedaOrigem=real'
 
-
-url_base = url[0:19]
+indice_interrogacao = url.find('?')
+print('Esse é o ínice: {}'.format(indice_interrogacao))
+url_base = url[0:indice_interrogacao]
 print(url_base)
 
-url_parametros = url[20:36]
+url_parametros = url[indice_interrogacao +1:]
 print(url_parametros)
 
-print(url)
+x = int(url.find('?'))
+print(x)
+resultado = ['Caractere encontrado', 'Caractere não encontrado'][x > 0]
+print(resultado)
+
