235c2e0e32dba0c21056139c56a8cfa8c0f02874
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2022-04-06 01:37:38 -0300

    Validação para pegar valores com múltiplos parâmetros

--- main.py
+++ main.py
@@ -3,27 +3,25 @@ url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dollar&quantida
 
 #url = 'bytebank.com/cambio?moedaOrigem=real'
 
+
+#separa base e os parametros
 indice_interrogacao = url.find('?')
 print('Esse é o ínice: {}'.format(indice_interrogacao))
 url_base = url[0:indice_interrogacao]
 print(url_base)
-
 url_parametros = url[indice_interrogacao +1:]
 print(url_parametros)
 
-#x = int(url.find('?'))
-#print(x)
-#resultado = ['Caractere encontrado', 'Caractere não encontrado'][x > 0]
-#print(resultado)
 
 
 
+# Busca o valor de um parametro
 parametro_busca = 'moedaOrigem'
 indice_parametro = url_parametros.find(parametro_busca)
-
-print(indice_parametro)
-
-
 indice_valor = indice_parametro + len(parametro_busca) + 1
-valor = url_parametros[indice_valor:16]
+indice_e_comercial = url_parametros.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
 print(valor)
