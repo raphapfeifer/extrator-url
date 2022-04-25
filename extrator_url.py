e56654b3b2a9491fd1fa334cf7f3fb1b0be185ea
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2022-04-25 01:44:09 -0300

    Refatoração do código

--- extrator_url.py
+++ extrator_url.py
@@ -10,11 +10,23 @@ class ExtratorURL:
         if self.url == "":
             raise ValueError("A URL está vazia")
 
+    def get_url_base(self):
+        indice_interrogacao = self.url.find('?')
+        url_base = self.url[0:indice_interrogacao]
+        return url_base
 
+    def get_url_parametros(self):
+        indice_interrogacao = self.url.find('?')
+        url_parametros = self.url[indice_interrogacao + 1:]
+        return url_parametros
 
+    def get_valor_parametro(self, parametro_busca):
+        indice_parametro = self.get_url_parametros().find(parametro_busca)
+        indice_valor = indice_parametro + len(parametro_busca) + 1
+        indice_e_comercial = self.get_url_parametros().find('&', indice_valor)
+        if indice_e_comercial == -1:
+            valor = self.get_url_parametros()[indice_valor:]
+        else:
+            valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
+        return valor
 
-
-
-extrator_url = ExtratorURL("  ")
-#extrator_url = ExtratorURL("https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dollar&quantidade=100")
-#valor_quantidade = extrator_url.get_valor_parametro("quantidade")
--- main.py
+++ main.py
@@ -1,28 +1,7 @@
-#url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dollar&quantidade=100"
-url = " "
+from extrator_url import ExtratorURL
 
-# Sanitização da URL
-#url = url.replace(" ", "")
+#extrator_url = ExtratorURL("https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dollar&quantidade=100")
+extrator_url = ExtratorURL(None)
+valor_quantidade = extrator_url.get_valor_parametro("quantidade")
 
-# Usando sanitização com o método strip
-url = url.strip()
-
-# Validação da URL
-if url == "":
-    raise ValueError("A url está vazia")
-
-# Separa base e os parametros
-indice_interrogacao = url.find('?')
-url_base = url[0:indice_interrogacao]
-url_parametros = url[indice_interrogacao +1:]
-
-# Busca o valor de um parametro
-parametro_busca = 'moedaDestino'
-indice_parametro = url_parametros.find(parametro_busca)
-indice_valor = indice_parametro + len(parametro_busca) + 1
-indice_e_comercial = url_parametros.find('&', indice_valor)
-if indice_e_comercial == -1:
-    valor = url_parametros[indice_valor:]
-else:
-    valor = url_parametros[indice_valor:indice_e_comercial]
-print(valor)
+print(valor_quantidade)
